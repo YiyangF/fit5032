1589e915d4b1ffbfea77fa71a635332c21320612
YIYANG FENG <yfen0064@student.monash.edu.com>
2024-08-02 20:38:17 +1000

    week 1

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>
+      My name is Yiyang Feng
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+.greetings h3 {
+  text-align: center;
+}
+
+</style>
