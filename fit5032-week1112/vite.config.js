1fe518e1205c40acaf8f7b4fc8d88371cda4d854
YIYANG FENG <yfen0064@student.monash.edu.com>
2024-10-18 19:16:07 +1100

    debug

--- fit5032-week1112/vite.config.js
+++ fit5032-week1112/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production'
+  base: import.meta.env.MODE === 'production'
     ? '/fit5032/'
     : '/'
 })
