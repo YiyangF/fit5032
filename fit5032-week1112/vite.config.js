0e0dc7e6f6373b1f6465eec9c55112f2cb128205
YIYANG FENG <yfen0064@student.monash.edu.com>
2024-10-18 19:12:13 +1100

    debug

--- fit5032-week1112/vite.config.js
+++ fit5032-week1112/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -14,11 +13,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/<fit5032>/'
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/fit5032/'
     : '/'
-}
+})
